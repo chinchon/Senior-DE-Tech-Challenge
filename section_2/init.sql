49e20e5fc3118f66a5953d30c6c0fbac68b975b4
ChinChon Na <chinchon91@gmail.com>
2023-03-06 02:29:00 +0800

    foreign keys

--- section_2/init.sql
+++ section_2/init.sql
@@ -3,15 +3,7 @@ create table members (
   first_name text,
   last_name text, 
   email text,
-  date_of_birth text
-);
-
-create table items (
-  item_id serial primary key,
-  item_name text, 
-  merchant_id int,
-  cost float,
-  weight_kg int
+  date_of_birth text  -- TODO store date_of_birth in timestamp type
 );
 
 create table merchants (
@@ -19,9 +11,21 @@ create table merchants (
   merchant_name text
 );
 
+create table items (
+  item_id serial primary key,
+  item_name text, 
+  merchant_id int,
+  FOREIGN KEY (merchant_id) REFERENCES merchants(merchant_id),
+  cost float,
+  weight_kg int
+);
+
 create table transactions (
   transaction_id serial primary key,
+  member_id text,
+  FOREIGN KEY (member_id) REFERENCES members(member_id),
   item_id int,
+  FOREIGN KEY (item_id) REFERENCES items(item_id),
   total_item_price float,
   total_itme_weight float
 );
